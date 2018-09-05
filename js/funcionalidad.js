c070652df6bbf98d666418d7ffa540a7523bb853
Jaime <Jaimeramirez14611@gmail.com>
2018-09-05 10:55:21 -0600

    Bug resuelto, error al redimencionar vMovil

--- js/funcionalidad.js
+++ js/funcionalidad.js
@@ -14,3 +14,31 @@ $("ul li a").on("click", function () {
     $("ul li").removeClass("activeCustom");
     $(this).addClass("activeCustom");
 });
+
+//cambia tamaño de los botones al cambiar de resolucion
+$(window).resize(function(){
+    if ($(window).width() <= 540) {  
+        $("section button").removeClass("btn  btn");
+        $("section button").addClass("btn  btn-xs");
+       // alert("kjk");
+           
+    }else{
+        $("section button").removeClass("btn  btn-xs");
+        $("section button").addClass("btn  btn");
+    }
+});
+
+//cabia tamaño de los botones 
+$(document).ready(function(){
+    if ($(window).width() <= 540) {  
+        $("section button").removeClass("btn  btn");
+        $("section button").addClass("btn  btn-xs");
+        //alert("si");
+           
+    }else{
+        $("section button").removeClass("btn  btn-xs");
+        $("section button").addClass("btn  btn");
+       // alert("no");
+    }
+
+    });
